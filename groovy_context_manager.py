06d8c93cef673b505c0207d396aa459cfd1c8e23
Chris Wright <chris@inkyspider.co.uk>
2021-03-31 23:28:32 +0100

    Add option to specify user specific prompts to messages

--- groovy_context_manager.py
+++ groovy_context_manager.py
@@ -10,10 +10,16 @@ def _looks_like_groovy_cmd(string):
     return re.match(r'^\s*-\s*\w+\s*', string) is not None
 
 
+class CustomClient(discord.Client):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.user_specific_prompts = {}
+
+
 logger = logging.getLogger('groovy context manager')
 intents = discord.Intents.default()
 intents.members = True
-client = discord.Client(intents=intents)
+client = CustomClient(intents=intents)
 
 
 @client.event
@@ -21,16 +27,15 @@ async def on_ready():
     logger.info(f'{client.user} connected to Discord!')
 
 
-async def warn_is_non_groovy_cmd(message):
-    await message.reply(
-        content=f"That message doesn't look like a groovy command, are you in the right channel?",
-        delete_after=10)
-
-
-async def warn_is_groovy_cmd(message):
-    await message.reply(
-        content=f"That message looks like a groovy command, are you in the right channel?",
-        delete_after=10)
+async def warn_groovy_cmd(message, positive=True, user_specific_prompts=None):
+    user_specific_prompts = user_specific_prompts or {}
+    verb = "looks" if positive else "doesn't look"
+    content = f"That message {verb} like a groovy command, "
+    if message.author.id in user_specific_prompts.keys():
+        content += user_specific_prompts[message.author.id]
+    else:
+        content += 'are you in the right channel?'
+    await message.reply(content=content, delete_after=10)
 
 
 @client.event
@@ -43,9 +48,9 @@ async def on_message(message):
     looks_like_groovy_cmd = _looks_like_groovy_cmd(message.content)
 
     if groovy_bot_in_channel and looks_like_groovy_text_channel and not looks_like_groovy_cmd:
-        await warn_is_non_groovy_cmd(message)
+        await warn_groovy_cmd(message, positive=False, user_specific_prompts=client.user_specific_prompts)
     elif not groovy_bot_in_channel and looks_like_groovy_cmd:
-        await warn_is_groovy_cmd(message)
+        await warn_groovy_cmd(message, user_specific_prompts=client.user_specific_prompts)
 
 
 if __name__ == '__main__':
